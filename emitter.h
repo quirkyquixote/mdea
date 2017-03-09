44e86998b45cfc0645541eca6c099a2713c4a222
Luis Sanz <luis.sanz@gmail.com>
2017-03-09 01:07:40 +0100

    Document emitter.h

--- emitter.h
+++ emitter.h
@@ -6,28 +6,41 @@
 #include <wchar.h>
 #include <stdlib.h>
 
+/*
+ * Functions for an emitter type
+ */
 struct mdea_emitter_type {
+	/* Destroy (but not free) */
 	void (*destroy)(void *);
+	/* Emit a number */
 	int (*emit_number)(void *, double number, wchar_t **error);
+	/* Emit a string */
 	int (*emit_string)(void *, const wchar_t *string, wchar_t **error);
 };
 
+/*
+ * Exposed fields for all node types
+ */
 struct mdea_emitter {
+	/* Pointer to type definition */
 	const struct mdea_emitter_type *type;
 };
 
+/* Destroy */
 static inline void mdea_emitter_destroy(struct mdea_emitter *e)
 {
 	e->type->destroy(e);
 	free(e);
 }
 
+/* Emit a number */
 static inline int mdea_emitter_emit_number(struct mdea_emitter *e,
 		double number, wchar_t **error)
 {
 	return e->type->emit_number(e, number, error);
 }
 
+/* Emit a string */
 static inline int mdea_emitter_emit_string(struct mdea_emitter *e,
 		const wchar_t *string, wchar_t **error)
 {
