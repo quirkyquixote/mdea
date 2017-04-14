e10745ca4b508bbdc548e967e3c111823e592230
Luis Sanz <luis.sanz@gmail.com>
2017-04-15 00:39:08 +0200

    Use the new wrapper functions in mdea-json.c
    The mdea-json tool uses now the wrapper functions added last commit.

--- src/mdea-json.c
+++ src/mdea-json.c
@@ -7,68 +7,6 @@
 
 #include "mdea.h"
 
-
-int node_from_file(const char *path, struct mdea_node **node, char **error)
-{
-	int ret = -1;
-	int file = open(path, O_RDONLY);
-	if (file >= 0) {
-		struct mdea_parser *p = mdea_file_parser(file);
-		struct mdea_emitter *e = mdea_node_emitter(node);
-		ret = mdea_parse(p, e, error);
-		mdea_parser_destroy(p);
-		mdea_emitter_destroy(e);
-		close(file);
-	} else {
-		perror(path);
-	}
-	return ret;
-}
-
-int node_from_string(const char *string, struct mdea_node **node, char **error)
-{
-	size_t len = strlen(string) + 3;
-	char tmp[len];
-	snprintf(tmp, len, "%s", string);
-	{
-		struct mdea_parser *p = mdea_string_parser(tmp);
-		struct mdea_emitter *e = mdea_node_emitter(node);
-		int ret = mdea_parse(p, e, NULL);
-		mdea_parser_destroy(p);
-		mdea_emitter_destroy(e);
-		if (ret == 0)
-			return 0;
-	}
-	snprintf(tmp, len, "\"%s\"", string);
-	{
-		struct mdea_parser *p = mdea_string_parser(tmp);
-		struct mdea_emitter *e = mdea_node_emitter(node);
-		int ret = mdea_parse(p, e, error);
-		mdea_parser_destroy(p);
-		mdea_emitter_destroy(e);
-		if (ret == 0)
-			return 0;
-	}
-	return -1;
-}
-
-int node_to_file(const char *path, struct mdea_node *node, char **error)
-{
-	int ret = -1;
-	int file = open(path, O_CREAT|O_WRONLY|O_TRUNC);
-	if (file >= 0) {
-		struct mdea_parser *p = mdea_node_parser(node);
-		struct mdea_emitter *e = mdea_file_emitter(file);
-		ret = mdea_write(e, node, error);
-		mdea_parser_destroy(p);
-		mdea_emitter_destroy(e);
-		close(file);
-	} else {
-		perror(path);
-	}
-	return ret;
-}
-
 int test(int argc, char *argv[], char **error)
 {
 	if (argc <= 1)
@@ -77,35 +15,27 @@ int test(int argc, char *argv[], char **error)
 		if (argc != 4)
 			return -1;
 		struct mdea_node *root;
-		if (node_from_file(argv[3], &root, error) != 0)
+		if (mdea_node_from_file_path(argv[3], &root, error) != 0)
 			return -1;
-		size_t len = strlen(argv[2]) + 1;
-		char path[len];
-		snprintf(path, len, "%s", argv[2]);
 		struct mdea_node *node;
-		if (mdea_get(root, path, &node, error) != 0)
+		if (mdea_get(root, argv[2], &node, error) != 0)
 			return -1;
-		struct mdea_emitter *e = mdea_file_emitter(STDOUT_FILENO);
-		if (mdea_write(e, node, error) != 0)
+		if (mdea_node_to_file_desc(node, STDOUT_FILENO, error) != 0)
 			return -1;
-		mdea_emitter_destroy(e);
 		return 0;
 	}
 	if (strcmp(argv[1], "set") == 0) {
 		if (argc != 5)
 			return -1;
 		struct mdea_node *root;
-		if (node_from_file(argv[4], &root, NULL) != 0)
+		if (mdea_node_from_file_path(argv[4], &root, NULL) != 0)
 			root = mdea_object_node();
 		struct mdea_node *node;
-		if (node_from_string(argv[3], &node, error) != 0)
+		if (mdea_node_from_string(argv[3], &node, error) != 0)
 			return -1;
-		size_t len = strlen(argv[2]) + 1;
-		char path[len];
-		snprintf(path, len, "%s", argv[2]);
-		if (mdea_set(&root, path, node, error) != 0)
+		if (mdea_set(&root, argv[2], node, error) != 0)
 			return -1;
-		return node_to_file(argv[4], root, error);
+		return mdea_node_to_file_path(root, argv[4], error);
 	}
 	return -1;
 }
